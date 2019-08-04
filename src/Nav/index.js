a750db843d17608cf5a186bb20be3f1b8d6ea5bb
pnxbill <pnxbill@gmail.com>
2019-08-04 00:22:00 -0300

    call api img and link, css adjustments

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,54 +1,50 @@
-import React, {Component} from 'react';
-import {Nav} from "./styles";
-import { withRouter } from "react-router-dom";
-import Button from "@material-ui/core/Button";
-
+import React, { Component } from 'react';
+import { Nav } from './styles';
+import { withRouter } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 class NavBar extends Component {
   constructor(props) {
     super(props);
-    this.state= {
-      user: '',
-    }
+    this.state = {
+      user: ''
+    };
   }
-  
-  
-  handleValue = ({target:{value, name}}) => {
-    this.setState({[name]: value})
-  };
-  
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.history.push(this.state.user)
-    this.setState({user: ''})
-    
-  };
-  
-  
-    render() {
-        return (
-            <Nav>
-              <form onSubmit={(e) => this.handleSubmit(e)}>
-                <input
-                  id="standard-multiline-flexible"
-                  label="Users"
-                  name={'user'}
-                  value={this.state.user}
-                  onChange={this.handleValue}
-              
-                /> 
-                
-                <Button onClick={(e)=> this.handleSubmit(e)} variant="contained" color="primary" >
-                  Primary
-                </Button>
-              </form>
-            </Nav>
 
-        );
-    }
+  handleValue = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { push } = this.props.history;
+    push(this.state.user);
+    this.setState({ user: '' });
+  };
+
+  render() {
+    return (
+      <Nav>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            id='standard-multiline-flexible'
+            label='Users'
+            name={'user'}
+            value={this.state.user}
+            onChange={this.handleValue}
+          />
+
+          <Button
+            onClick={e => this.handleSubmit(e)}
+            variant='contained'
+            color='primary'
+          >
+            Primary
+          </Button>
+        </form>
+      </Nav>
+    );
+  }
 }
 
-
-
 export default withRouter(NavBar);
-
--- src/Users/index.js
+++ src/Users/index.js
@@ -1,26 +1,41 @@
-import React, {Component} from 'react';
-import { withRouter } from "react-router-dom";
-import {loadDataRequest} from '../actions'
-import {connect} from "react-redux";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { loadDataRequest } from '../actions';
+import { connect } from 'react-redux';
 
 class User extends Component {
-  
   componentDidMount() {
-    const user = this.props.match.params.nameUser
-    this.props.loadDataRequest(user)
+    const user = this.props.match.params.nameUser;
+    this.props.loadDataRequest(user);
   }
 
   render() {
     return (
-      <div style={{marginLeft: '50%', marginTop: '10%'}}>
-        {this.props.user.map((item) => {
-          return <div>User: {item.login}</div>
+      <div style={{ textAlign: 'center', marginTop: '10%' }}>
+        {this.props.user.map(item => {
+          return (
+            <div>
+              <img
+                src={item.avatar_url}
+                style={{
+                  maxWidth: '200px',
+                  maxHeight: '200px',
+                  border: '1px solid black',
+                  borderRadius: '50%'
+                }}
+                alt={`avatar-${item.login}`}
+              />
+              <a href={item.html_url} target='_blank' rel='noopener noreferrer'>
+                {' '}
+                <h2>User: {item.login}</h2>
+              </a>
+            </div>
+          );
         })}
       </div>
     );
   }
 }
-
 
 function mapStateToProps(state) {
   return {
@@ -29,8 +44,7 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps,{loadDataRequest})(withRouter(User));
-
-
-
-
+export default connect(
+  mapStateToProps,
+  { loadDataRequest }
+)(withRouter(User));
